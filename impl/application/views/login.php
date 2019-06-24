b447ef026d4554e582c010be0a7527bc2a8179fa
Jeckaa <jeca.cvetinovic998@gmail.com>
2019-06-24 20:02:36 +0200

    Resen bootstrap problem

--- impl/application/views/login.php
+++ impl/application/views/login.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="bootstrap.css"> 
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url('bootstrap/css/bootstrap.css');?>"> 
     </head>
     <body>
         <div class="centered">
